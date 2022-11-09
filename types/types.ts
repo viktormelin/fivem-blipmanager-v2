53b1f238e5c613eb8f8b2e9f3c173ffddd4ac794
dough <viktor.melin@me.com>
2022-11-09 18:27:30 +0100

    refactor(typings): added partial for config, settings

--- types/types.ts
+++ types/types.ts
@@ -1,26 +1,41 @@
+export type DeepPartial<T> = T extends object ? {
+  [P in keyof T]?: DeepPartial<T[P]>;
+} : T;
+
+export type TCoord = { x: number, y: number, z: number, w?: number }
+
 export interface IFetchedData {
-    categories: ICategory[],
-    blips: IBlipData[]
+  categories: ICategory[],
+  blips: IBlipData[]
+}
+
+export interface IBlipSettings {
+  coords: TCoord,
+  sprite: number,
+  shortRange: boolean,
+  scale: number,
+  colour: number,
+  label: string
 }
 
 export interface IBlipData {
-    id: number,
-    blips: number[],
-    category: string,
-    label: string,
-    description?: string,
-    currentState: boolean,
+  unique: string,
+  blips: number[],
+  settings: IBlipSettings
+  category: string,
+  label: string,
+  description?: string,
+  currentState: boolean,
 }
 
 export interface ICategory {
-    name: string,
-    label: string,
-    description?: string
+  name: string,
+  label: string,
+  description?: string
 }
 
 export interface IConfig {
-    categories: ICategory[],
-    showBlipCount: boolean,
-    useCommand: boolean,
-    commandName?: string
+  categories: ICategory[],
+  showBlipCount: boolean,
+  useCommand: boolean,
 }
